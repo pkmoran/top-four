e1c9b3ed3917f0c58f1bf62b8d0c6320d53dc08d
Andrew Sutherland <azsuth@gmail.com>
2019-04-30 10:44:56 -0500

    removed offline functionality

--- web/src/index.js
+++ web/src/index.js
@@ -4,7 +4,7 @@ import { Route } from 'react-router-dom';
 import './index.css';
 import Root from './components/Root';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Root>
@@ -13,4 +13,7 @@ ReactDOM.render(
   document.querySelector('#root')
 );
 
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
