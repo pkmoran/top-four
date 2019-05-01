b5f3d900b9ec1f28f89a5b2bfd6c63346e5f63d4
Andrew Sutherland <azsuth@gmail.com>
2019-04-30 19:21:35 -0500

    added rewrite config for firebase deployment

--- web/src/components/LandingPage/LandingPage.js
+++ web/src/components/LandingPage/LandingPage.js
@@ -68,7 +68,7 @@ class LandingPageComponent extends Component {
   render() {
     return (
       <div className="LandingPage">
-        <h1>Top Four TM v1.6</h1>
+        <h1>Top Four TM v1.7</h1>
 
         <TextField
           onChange={this.props.nameChanged}
