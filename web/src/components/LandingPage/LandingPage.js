69d995ccca50de657b6982b6aa102cea811d5a42
Andrew Sutherland <azsuth@gmail.com>
2019-05-03 14:22:29 -0500

    v1.10.1

--- web/src/components/LandingPage/LandingPage.js
+++ web/src/components/LandingPage/LandingPage.js
@@ -71,7 +71,7 @@ class LandingPageComponent extends Component {
   render() {
     return (
       <div className="LandingPage">
-        <h1>Top Four TM v1.10</h1>
+        <h1>Top Four TM v1.10.1</h1>
 
         <TextField
           onChange={this.props.nameChanged}
