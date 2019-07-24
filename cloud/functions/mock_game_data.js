c433a8021a300fd21f209bb592264e2394a0053e
Andrew Sutherland <azsuth@gmail.com>
2019-07-24 17:21:25 -0500

    added superlatives package

---
+++ cloud/functions/mock_game_data.js
@@ -0,0 +1,595 @@
+exports.data = {
+  gameId: 'R2',
+  guesses: {
+    '-LkU9sp6tiBeHIJUO8zg': {
+      '-LkU9sj5W5cffnMMKnKM': 'active',
+      '-LkU9sj5W5cffnMMKnKO': 'active',
+      '-LkU9sj92QS0_-xVtGP4': 'active',
+      '-LkU9sjFdM5VynoewW5C': 3,
+      '-LkU9sk932Qan7fufUVm': 1,
+      '-LkU9skAeGlz61mKX_S5': 2,
+      '-LkU9sloiUDehIOLp6Oc': 'active',
+      '-LkU9sloiUDehIOLp6Od': 0
+    },
+    '-LkUAe319vLqhDHiNEtJ': {
+      '-LkU9sj5W5cffnMMKnKM': 3,
+      '-LkU9sj5W5cffnMMKnKO': 0,
+      '-LkU9sj92QS0_-xVtGP4': 1,
+      '-LkU9sjFdM5VynoewW5C': 'active',
+      '-LkU9sk932Qan7fufUVm': 'active',
+      '-LkU9skAeGlz61mKX_S5': 'active',
+      '-LkU9sloiUDehIOLp6Oc': 2,
+      '-LkU9sloiUDehIOLp6Od': 'active'
+    },
+    '-LkUAh7VGipfAZtpOyOW': {
+      '-LkU9sj5W5cffnMMKnKM': 3,
+      '-LkU9sj5W5cffnMMKnKO': 0,
+      '-LkU9sj92QS0_-xVtGP4': 1,
+      '-LkU9sjFdM5VynoewW5C': 0,
+      '-LkU9sk932Qan7fufUVm': 1,
+      '-LkU9skAeGlz61mKX_S5': 2,
+      '-LkU9sloiUDehIOLp6Oc': 2,
+      '-LkU9sloiUDehIOLp6Od': 3
+    }
+  },
+  players: {
+    '-LkU9sp6tiBeHIJUO8zg': {
+      lockedIn: false,
+      name: 'Scuzz',
+      score: 0,
+      teamUid: '-LkU9shHWibRWwJddHbE'
+    },
+    '-LkU9tksZl25ZiruKuX1': {
+      lockedIn: false,
+      name: 'asdf',
+      score: 0
+    },
+    '-LkUAe319vLqhDHiNEtJ': {
+      lockedIn: false,
+      name: 'asdf',
+      score: 2,
+      teamUid: '-LkU9shG-w8c91Fi10lD'
+    },
+    '-LkUAh7VGipfAZtpOyOW': {
+      lockedIn: false,
+      name: 'qwer',
+      score: 4,
+      teamUid: '-LkU9shG-w8c91Fi10lD'
+    }
+  },
+  rankingPlayerUid: '',
+  rankingTeamUid: '-LkU9shG-w8c91Fi10lD',
+  startDate: '2019-07-23T14:10:43+00:00',
+  state: '',
+  teams: {
+    '-LkU9shG-w8c91Fi10lD': {
+      name: 'Team 1'
+    },
+    '-LkU9shHWibRWwJddHbE': {
+      name: 'Team 2'
+    }
+  },
+  topicPack: true,
+  topics: {
+    '-LkU9sisdSLiqwPpykrZ': {
+      rank: -1,
+      status: 'available',
+      topic: 'Dave Matthews Band'
+    },
+    '-LkU9sisdSLiqwPpykr_': {
+      rank: -1,
+      status: 'available',
+      topic: 'Nikelback'
+    },
+    '-LkU9sitKKmOZGXAfRCR': {
+      rank: -1,
+      status: 'available',
+      topic: 'Country Music'
+    },
+    '-LkU9sitKKmOZGXAfRCS': {
+      rank: -1,
+      status: 'available',
+      topic: 'Marvel movies'
+    },
+    '-LkU9sitKKmOZGXAfRCT': {
+      rank: -1,
+      status: 'available',
+      topic: 'Reality TV'
+    },
+    '-LkU9siubuwGMnC9n1uq': {
+      rank: -1,
+      status: 'available',
+      topic: 'Tinder'
+    },
+    '-LkU9siubuwGMnC9n1ur': {
+      rank: -1,
+      status: 'available',
+      topic: 'DIY'
+    },
+    '-LkU9siubuwGMnC9n1us': {
+      rank: -1,
+      status: 'available',
+      topic: '💩'
+    },
+    '-LkU9siubuwGMnC9n1ut': {
+      rank: -1,
+      status: 'available',
+      topic: 'The Royal Wedding'
+    },
+    '-LkU9siy_-OsbYNpkpK6': {
+      rank: -1,
+      status: 'available',
+      topic: 'Facebook'
+    },
+    '-LkU9sj3JWWf5l_I2M6k': {
+      rank: -1,
+      status: 'available',
+      topic: 'Twitter'
+    },
+    '-LkU9sj4A1DBcA3-51EU': {
+      rank: -1,
+      status: 'available',
+      topic: 'Memes'
+    },
+    '-LkU9sj4A1DBcA3-51EV': {
+      rank: -1,
+      status: 'available',
+      topic: 'Dog instagram accounts'
+    },
+    '-LkU9sj4A1DBcA3-51EW': {
+      rank: -1,
+      status: 'available',
+      topic: 'Road trips'
+    },
+    '-LkU9sj5W5cffnMMKnKM': {
+      rank: 3,
+      status: 'unavailable',
+      topic: 'Pimple-popping videos'
+    },
+    '-LkU9sj5W5cffnMMKnKN': {
+      rank: -1,
+      status: 'available',
+      topic: 'Music fesivals'
+    },
+    '-LkU9sj5W5cffnMMKnKO': {
+      rank: 0,
+      status: 'unavailable',
+      topic: 'Cats'
+    },
+    '-LkU9sj6uVcpCa_blhll': {
+      rank: -1,
+      status: 'available',
+      topic: 'Las Vegas'
+    },
+    '-LkU9sj6uVcpCa_blhlm': {
+      rank: -1,
+      status: 'available',
+      topic: 'Deserts'
+    },
+    '-LkU9sj6uVcpCa_blhln': {
+      rank: -1,
+      status: 'available',
+      topic: 'Wearing pants without a belt'
+    },
+    '-LkU9sj6uVcpCa_blhlo': {
+      rank: -1,
+      status: 'available',
+      topic: 'Uber'
+    },
+    '-LkU9sj8YEZqLzDnHE_1': {
+      rank: -1,
+      status: 'available',
+      topic: 'Podcasts'
+    },
+    '-LkU9sj92QS0_-xVtGP3': {
+      rank: -1,
+      status: 'available',
+      topic: 'Game of Thrones'
+    },
+    '-LkU9sj92QS0_-xVtGP4': {
+      rank: 2,
+      status: 'unavailable',
+      topic: 'Live action Disney remakes'
+    },
+    '-LkU9sjENbbu6YNKmXrv': {
+      rank: -1,
+      status: 'available',
+      topic: 'Kanye West'
+    },
+    '-LkU9sjENbbu6YNKmXrw': {
+      rank: -1,
+      status: 'available',
+      topic: 'The Kardashians'
+    },
+    '-LkU9sjFdM5VynoewW5C': {
+      rank: 0,
+      status: 'unavailable',
+      topic: 'Joseph Gordon Levitt'
+    },
+    '-LkU9sjFdM5VynoewW5D': {
+      rank: -1,
+      status: 'available',
+      topic: 'Jimmy Fallon'
+    },
+    '-LkU9sk8EvKQtJdfzJlx': {
+      rank: -1,
+      status: 'available',
+      topic: 'Ira Glass'
+    },
+    '-LkU9sk932Qan7fufUVl': {
+      rank: -1,
+      status: 'available',
+      topic: 'Drake'
+    },
+    '-LkU9sk932Qan7fufUVm': {
+      rank: 2,
+      status: 'unavailable',
+      topic: 'Taylor Swift'
+    },
+    '-LkU9skAeGlz61mKX_S3': {
+      rank: -1,
+      status: 'available',
+      topic: 'Lena Dunham'
+    },
+    '-LkU9skAeGlz61mKX_S4': {
+      rank: -1,
+      status: 'available',
+      topic: 'Amy Schumer'
+    },
+    '-LkU9skAeGlz61mKX_S5': {
+      rank: 1,
+      status: 'unavailable',
+      topic: 'Aziz Ansari'
+    },
+    '-LkU9skBtJWlfpux36PK': {
+      rank: -1,
+      status: 'available',
+      topic: 'Alec Baldwin'
+    },
+    '-LkU9skBtJWlfpux36PL': {
+      rank: -1,
+      status: 'available',
+      topic: 'Ben Affleck'
+    },
+    '-LkU9skBtJWlfpux36PM': {
+      rank: -1,
+      status: 'available',
+      topic: 'Matt Damon'
+    },
+    '-LkU9skBtJWlfpux36PN': {
+      rank: -1,
+      status: 'available',
+      topic: 'Ke$ha'
+    },
+    '-LkU9skCiGeUaztkJ2CH': {
+      rank: -1,
+      status: 'available',
+      topic: 'Natalie Portman'
+    },
+    '-LkU9skCiGeUaztkJ2CI': {
+      rank: -1,
+      status: 'available',
+      topic: 'Tom Brady'
+    },
+    '-LkU9skDYCuaNEYDOm68': {
+      rank: -1,
+      status: 'available',
+      topic: 'Nicholas Cage'
+    },
+    '-LkU9skEqnreOuIMLFmb': {
+      rank: -1,
+      status: 'available',
+      topic: 'Chris Pratt'
+    },
+    '-LkU9skEqnreOuIMLFmc': {
+      rank: -1,
+      status: 'available',
+      topic: 'Robert Downey Jr.'
+    },
+    '-LkU9skEqnreOuIMLFmd': {
+      rank: -1,
+      status: 'available',
+      topic: 'Johnny Depp'
+    },
+    '-LkU9skFa4uUZupb5Gty': {
+      rank: -1,
+      status: 'available',
+      topic: 'Jennifer Lawrence'
+    },
+    '-LkU9skFa4uUZupb5Gtz': {
+      rank: -1,
+      status: 'available',
+      topic: 'Tina Fey'
+    },
+    '-LkU9skGGqeV_mtPgS3O': {
+      rank: -1,
+      status: 'available',
+      topic: 'Kenan Thompson'
+    },
+    '-LkU9skGGqeV_mtPgS3P': {
+      rank: -1,
+      status: 'available',
+      topic: 'Michael Che'
+    },
+    '-LkU9skHuhYRsguaed0l': {
+      rank: -1,
+      status: 'available',
+      topic: 'Biting stringing cheese instead of stringing it'
+    },
+    '-LkU9skHuhYRsguaed0m': {
+      rank: -1,
+      status: 'available',
+      topic: 'LaCroix'
+    },
+    '-LkU9skHuhYRsguaed0n': {
+      rank: -1,
+      status: 'available',
+      topic: 'Seafood'
+    },
+    '-LkU9skI5b86RKfXywe6': {
+      rank: -1,
+      status: 'available',
+      topic: 'Eggs'
+    },
+    '-LkU9skI5b86RKfXywe7': {
+      rank: -1,
+      status: 'available',
+      topic: 'Tomatos'
+    },
+    '-LkU9skJyvM9U1kI_CwM': {
+      rank: -1,
+      status: 'available',
+      topic: 'Coffee'
+    },
+    '-LkU9skJyvM9U1kI_CwN': {
+      rank: -1,
+      status: 'available',
+      topic: 'Spicy food'
+    },
+    '-LkU9skKfHevT1u_Uklc': {
+      rank: -1,
+      status: 'available',
+      topic: 'Tex-Mex'
+    },
+    '-LkU9skKfHevT1u_Ukld': {
+      rank: -1,
+      status: 'available',
+      topic: 'Cilantro'
+    },
+    '-LkU9skLOu-tMJ_-JVhJ': {
+      rank: -1,
+      status: 'available',
+      topic: 'Marshmallow Peeps'
+    },
+    '-LkU9skLOu-tMJ_-JVhK': {
+      rank: -1,
+      status: 'available',
+      topic: 'White chocolate'
+    },
+    '-LkU9skMnjQucBz6y8G4': {
+      rank: -1,
+      status: 'available',
+      topic: 'Choco Tacos'
+    },
+    '-LkU9skMnjQucBz6y8G5': {
+      rank: -1,
+      status: 'available',
+      topic: 'Plant-based diets'
+    },
+    '-LkU9skNTz4QeXvpBGcA': {
+      rank: -1,
+      status: 'available',
+      topic: 'Ramen'
+    },
+    '-LkU9skNTz4QeXvpBGcB': {
+      rank: -1,
+      status: 'available',
+      topic: 'Gin'
+    },
+    '-LkU9skNTz4QeXvpBGcC': {
+      rank: -1,
+      status: 'available',
+      topic: 'Candy hearts'
+    },
+    '-LkU9skO-ifL6pTCwEWd': {
+      rank: -1,
+      status: 'available',
+      topic: 'Socks with sandals'
+    },
+    '-LkU9skO-ifL6pTCwEWe': {
+      rank: -1,
+      status: 'available',
+      topic: 'Cargo Shorts'
+    },
+    '-LkU9skO-ifL6pTCwEWf': {
+      rank: -1,
+      status: 'available',
+      topic: 'Biting your nails'
+    },
+    '-LkU9skPyaWXFoWwQXVJ': {
+      rank: -1,
+      status: 'available',
+      topic: 'Recycling'
+    },
+    '-LkU9skPyaWXFoWwQXVK': {
+      rank: -1,
+      status: 'available',
+      topic: 'Wearing white after Labor Day'
+    },
+    '-LkU9skPyaWXFoWwQXVL': {
+      rank: -1,
+      status: 'available',
+      topic: 'Staying out past midnight'
+    },
+    '-LkU9slhCYClIn-g2uvb': {
+      rank: -1,
+      status: 'available',
+      topic: 'Coasters'
+    },
+    '-LkU9sliYEwUvJ3w1t-E': {
+      rank: -1,
+      status: 'available',
+      topic: 'Group texts'
+    },
+    '-LkU9sliYEwUvJ3w1t-F': {
+      rank: -1,
+      status: 'available',
+      topic: 'Lunch dates'
+    },
+    '-LkU9slj2jUkgmbIRQ3r': {
+      rank: -1,
+      status: 'available',
+      topic: 'Spray tans'
+    },
+    '-LkU9slj2jUkgmbIRQ3s': {
+      rank: -1,
+      status: 'available',
+      topic: '“Moist”'
+    },
+    '-LkU9slkCoX_Km_PiLMK': {
+      rank: -1,
+      status: 'available',
+      topic: '“Cheers!” as an email sign-off'
+    },
+    '-LkU9sllRJZRrE_Cg2Wq': {
+      rank: -1,
+      status: 'available',
+      topic: 'Washing your hands with soap after peeing'
+    },
+    '-LkU9sllRJZRrE_Cg2Wr': {
+      rank: -1,
+      status: 'available',
+      topic: 'Cologne'
+    },
+    '-LkU9sllRJZRrE_Cg2Ws': {
+      rank: -1,
+      status: 'available',
+      topic: 'Dog-friendly offices'
+    },
+    '-LkU9slmecbE0VzKKIcl': {
+      rank: -1,
+      status: 'available',
+      topic: 'Scary movies'
+    },
+    '-LkU9slmecbE0VzKKIcm': {
+      rank: -1,
+      status: 'available',
+      topic: 'Nonfiction books'
+    },
+    '-LkU9slnAxsK6jywD1gA': {
+      rank: -1,
+      status: 'available',
+      topic: 'Soccer'
+    },
+    '-LkU9slnAxsK6jywD1gB': {
+      rank: -1,
+      status: 'available',
+      topic: "Smelling your armpit to see if it's fresh"
+    },
+    '-LkU9slnAxsK6jywD1gC': {
+      rank: -1,
+      status: 'available',
+      topic: 'Singing "Happy Birthday" at a restaurant'
+    },
+    '-LkU9sloiUDehIOLp6Ob': {
+      rank: -1,
+      status: 'available',
+      topic: 'Meal prepping'
+    },
+    '-LkU9sloiUDehIOLp6Oc': {
+      rank: 1,
+      status: 'unavailable',
+      topic: 'Sleeping in'
+    },
+    '-LkU9sloiUDehIOLp6Od': {
+      rank: 3,
+      status: 'unavailable',
+      topic: 'Chewing ice'
+    },
+    '-LkU9slpfUykuRPRJHe3': {
+      rank: -1,
+      status: 'available',
+      topic: 'High fives at work'
+    },
+    '-LkU9slpfUykuRPRJHe4': {
+      rank: -1,
+      status: 'available',
+      topic: 'Election season'
+    },
+    '-LkU9slr7nNrhJLdm-E1': {
+      rank: -1,
+      status: 'available',
+      topic: 'Mornings'
+    },
+    '-LkU9slr7nNrhJLdm-E2': {
+      rank: -1,
+      status: 'available',
+      topic: 'Black Friday'
+    },
+    '-LkU9slsDfh83E-VX2Qz': {
+      rank: -1,
+      status: 'available',
+      topic: 'Black tie weddings'
+    },
+    '-LkU9slsDfh83E-VX2R-': {
+      rank: -1,
+      status: 'available',
+      topic: 'Halloween Parties'
+    },
+    '-LkU9sltNBKjKb706vit': {
+      rank: -1,
+      status: 'available',
+      topic: 'The Winter Olympics'
+    },
+    '-LkU9sltNBKjKb706viu': {
+      rank: -1,
+      status: 'available',
+      topic: 'Christmas music in months other than December'
+    },
+    '-LkU9sltNBKjKb706viv': {
+      rank: -1,
+      status: 'available',
+      topic: 'White elephants'
+    },
+    '-LkU9sluRI4839T-dWcD': {
+      rank: -1,
+      status: 'available',
+      topic: 'Haunted houses'
+    },
+    '-LkU9sluRI4839T-dWcE': {
+      rank: -1,
+      status: 'available',
+      topic: 'Cruises'
+    },
+    '-LkU9sluRI4839T-dWcF': {
+      rank: -1,
+      status: 'available',
+      topic: 'Dry resorts'
+    },
+    '-LkU9slvFQVWH3AzAMBE': {
+      rank: -1,
+      status: 'available',
+      topic: "Attending your sibling's graduation"
+    },
+    '-LkU9slvFQVWH3AzAMBF': {
+      rank: -1,
+      status: 'available',
+      topic: 'Company retreats'
+    },
+    '-LkU9slvFQVWH3AzAMBG': {
+      rank: -1,
+      status: 'available',
+      topic: 'The Academy Awards'
+    },
+    '-LkU9slvFQVWH3AzAMBH': {
+      rank: -1,
+      status: 'available',
+      topic: 'Netflix Comedy Specials'
+    },
+    '-LkU9slwkH3r3CcdSXdX': {
+      rank: -1,
+      status: 'available',
+      topic: 'The World Series'
+    }
+  }
+};
---
+++ cloud/functions/superlatives.js
@@ -0,0 +1,228 @@
+const data = require('./mock_game_data').data;
+
+const players = {
+  array: map(data.players, (player, uid) => ({ ...player, uid }))
+};
+
+const theWinningTeam = superlatives => {
+  const teamScores = Object.keys(data.players).reduce((teams, playerUid) => {
+    const player = data.players[playerUid];
+
+    if (!player.teamUid) {
+      return teams;
+    }
+
+    return {
+      ...teams,
+      [player.teamUid]: (teams[player.teamUid] || 0) + player.score
+    };
+  }, {});
+
+  const winningTeamUid = Object.keys(teamScores).reduce((winningUid, teamUid) =>
+    teamScores[winningUid] > teamScores[teamUid] ? winningUid : teamUid
+  );
+
+  superlatives.push({
+    title: 'The Winner',
+    receivedBy: data.teams[winningTeamUid].name,
+    description: 'Winner, winner, chicken dinner'
+  });
+};
+
+const theExpert = superlatives => {
+  const player = players.array.reduce((winning, player) =>
+    winning.score > player.score ? winning : player
+  );
+
+  superlatives.push({
+    title: 'The Expert',
+    receivedBy: player.name,
+    description: `Congrats! You know the most about how people feel about random, trivial things. Oh, the places you'll go.`
+  });
+};
+
+const theIndividualTopicAwards = superlatives => {
+  const topicGuesses = Object.keys(data.guesses).reduce(
+    (topicGuesses, playerUid) => {
+      Object.keys(data.guesses[playerUid]).forEach(topicUid => {
+        if (!topicGuesses[topicUid]) {
+          topicGuesses[topicUid] = {
+            correctRank: data.topics[topicUid].rank,
+            guesses: [],
+            ranker: undefined,
+            numberCorrect: 0,
+            numberIncorrect: 0
+          };
+        }
+
+        if (data.guesses[playerUid][topicUid] === 'active') {
+          topicGuesses[topicUid].ranker = playerUid;
+        } else {
+          const guess = data.guesses[playerUid][topicUid];
+
+          topicGuesses[topicUid].guesses.push(guess);
+
+          if (guess === topicGuesses[topicUid].correctRank) {
+            topicGuesses[topicUid].numberCorrect += 1;
+          } else {
+            topicGuesses[topicUid].numberIncorrect += 1;
+          }
+        }
+      });
+
+      return topicGuesses;
+    },
+    {}
+  );
+
+  const rankers = Object.keys(topicGuesses).reduce((rankers, topicUid) => {
+    const topicGuess = topicGuesses[topicUid];
+    const rankerUid = topicGuess.ranker;
+
+    if (!rankers[rankerUid]) {
+      rankers[rankerUid] = {
+        numberCorrect: 0,
+        numberLied: 0,
+        numberPerfect: 0
+      };
+    }
+
+    rankers[rankerUid].numberCorrect += topicGuess.numberCorrect;
+
+    if (
+      topicGuess.numberCorrect === 0 &&
+      new Set(topicGuess.guesses).size === 1
+    ) {
+      rankers[rankerUid].numberLied += 1;
+    }
+
+    if (topicGuess.numberIncorrect === 0) {
+      rankers[rankerUid].numberPerfect += 1;
+    }
+
+    return rankers;
+  }, {});
+
+  const lowestRankerUid = Object.keys(rankers).reduce((lowestUid, rankerUid) =>
+    rankers[lowestUid].numberCorrect < rankers[rankerUid].numberCorrect
+      ? lowestUid
+      : rankerUid
+  );
+
+  const mostLiedUid = Object.keys(rankers).reduce((mostUid, rankerUid) =>
+    rankers[mostUid].numberLied > rankers[rankerUid].numberLied
+      ? mostUid
+      : rankerUid
+  );
+
+  const mostPerfectUid = Object.keys(rankers).reduce((mostUid, rankerUid) =>
+    rankers[mostUid].numberPerfect > rankers[rankerUid].numberPerfect
+      ? mostUid
+      : rankerUid
+  );
+
+  superlatives.push({
+    title: 'The Complete Stranger',
+    receivedBy: data.players[lowestRankerUid].name,
+    description: `Do you know anyone here? Because they don't know you. They only guessed ${
+      rankers[lowestRankerUid].numberCorrect
+    } of your ranks correctly.`
+  });
+
+  superlatives.push({
+    title: 'The Liar',
+    receivedBy: data.players[mostLiedUid].name,
+    description: `Can you just tell the truth next time? Because the group agreed about you ${
+      rankers[mostLiedUid].numberLied
+    } times, but never seemed to get it right.`
+  });
+
+  superlatives.push({
+    title: 'The Open Book',
+    receivedBy: data.players[mostPerfectUid].name,
+    description: `The group guessed you 100% correct ${
+      rankers[mostPerfectUid].numberPerfect
+    } times, time to add some mystery to your life.`
+  });
+};
+
+const theDisagreeTeam = superlatives => {
+  const teamPlayers = Object.keys(data.players).reduce(
+    (teamPlayers, playerUid) => {
+      const player = data.players[playerUid];
+
+      if (!player.teamUid) {
+        return teamPlayers;
+      }
+
+      if (!teamPlayers[player.teamUid]) {
+        teamPlayers[player.teamUid] = {
+          players: [],
+          topics: {}
+        };
+      }
+
+      const topics = Object.keys(data.guesses[playerUid]).reduce(
+        (topics, topicUid) => {
+          if (data.guesses[playerUid][topicUid] === 'active') {
+            return topics;
+          }
+
+          return {
+            ...topics,
+            [topicUid]: new Set([
+              ...(topics[topicUid] || []),
+              data.guesses[playerUid][topicUid]
+            ])
+          };
+        },
+        teamPlayers[player.teamUid].topics
+      );
+
+      teamPlayers[player.teamUid].players.push(player);
+      teamPlayers[player.teamUid].topics = topics;
+
+      return teamPlayers;
+    },
+    {}
+  );
+
+  const teamDisagrees = Object.keys(teamPlayers).reduce(
+    (teamDisagrees, teamUid) => {
+      const team = teamPlayers[teamUid];
+
+      const disagrees = Object.keys(team.topics).reduce(
+        (disagrees, topicUid) => disagrees + team.topics[topicUid].size - 1,
+        0
+      );
+
+      return {
+        ...teamDisagrees,
+        [teamUid]: disagrees
+      };
+    },
+    {}
+  );
+
+  const mostDisagreeUid = Object.keys(teamDisagrees).reduce(
+    (mostUid, teamUid) =>
+      teamDisagrees[mostUid] > teamDisagrees[teamUid] ? mostUid : teamUid
+  );
+
+  superlatives.push({
+    title: 'The Disagree Team',
+    receivedBy: data.teams[mostDisagreeUid].name,
+    description: `Your team disagreed ${
+      teamDisagrees[mostDisagreeUid]
+    } times! Maybe you should seek counseling...`
+  });
+};
+
+const superlatives = [];
+
+theWinningTeam(superlatives);
+theExpert(superlatives);
+theIndividualTopicAwards(superlatives);
+theDisagreeTeam(superlatives);
+
+console.log(superlatives);
