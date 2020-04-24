016bed7e2f1939c39073595361c2e975fb1e1b93
Andrew Sutherland <azsuth@gmail.com>
2020-04-23 21:55:12 -0500

    removed firebase analytics for now

--- web-2/src/services/logger.js
+++ web-2/src/services/logger.js
@@ -1,15 +1,5 @@
-import firebase from 'firebase/app';
-import 'firebase/analytics';
-
 const logError = (error, path) => {
-  if (
-    process.env.NODE_ENV === 'development' ||
-    process.env.NODE_ENV === 'test'
-  ) {
-    console.log('logError', error, path);
-  } else {
-    firebase.analytics().logEvent('error', { path, error });
-  }
+  console.log('logError', error, path);
 };
 
 export { logError };
