0ac078ab1eb071ea90e8dc707e6553436aa6fac2
Andrew Sutherland <azsuth@gmail.com>
2020-04-24 23:46:07 -0500

    one-lined the header

--- web-2/src/components/game/header.js
+++ web-2/src/components/game/header.js
@@ -7,15 +7,14 @@ import { withState } from '@state';
 const Header = ({ round, totalRounds, gameId, onClickScores }) => {
   return (
     <div class="game-header">
-      <div class="game-header__top">
-        <span>
-          Round {round}/{totalRounds}
+      <span>
+        <span class="game-header__game-id">{gameId}</span>{' '}
+        <span class="game-header__rounds">
+          &bull; Round {round}/{totalRounds}
         </span>
-        <span class="game-header__game-id">{gameId}</span>
-      </div>
-      <span class="game-header__bottom">
-        <a onClick={onClickScores}>SCORES-></a>
       </span>
+
+      <a onClick={onClickScores}>SCORES-></a>
     </div>
   );
 };
