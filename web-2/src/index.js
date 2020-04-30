80ca786dd20ec9759c96ba13103151510de46e59
Andrew Sutherland <azsuth@gmail.com>
2020-04-29 21:49:55 -0500

    move firebase initialization

--- web-2/src/index.js
+++ web-2/src/index.js
@@ -1,5 +1,3 @@
-import firebase from 'firebase/app';
-import 'firebase/database';
 import { ThemeProvider } from '@material-ui/core';
 
 import { GameStateProvider } from '@state';
@@ -11,16 +9,6 @@ import 'style';
 import 'typeface-roboto';
 
 import theme from 'style/theme';
-
-firebase.initializeApp({
-  apiKey: process.env.TOP_FOUR_FIREBASE_API_KEY,
-  authDomain: 'top-four-cca25.firebaseapp.com',
-  databaseURL: 'https://top-four-cca25.firebaseio.com',
-  projectId: 'top-four-cca25',
-  storageBucket: 'top-four-cca25.appspot.com',
-  messagingSenderId: '120019969623',
-  appId: '1:120019969623:web:6d6ba9a3d0834b0259e512'
-});
 
 const Root = () => {
   const state = loadState();
--- web-2/src/services/index.js
+++ web-2/src/services/index.js
@@ -2,6 +2,16 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 import 'firebase/functions';
 
+firebase.initializeApp({
+  apiKey: process.env.TOP_FOUR_FIREBASE_API_KEY,
+  authDomain: 'top-four-cca25.firebaseapp.com',
+  databaseURL: 'https://top-four-cca25.firebaseio.com',
+  projectId: 'top-four-cca25',
+  storageBucket: 'top-four-cca25.appspot.com',
+  messagingSenderId: '120019969623',
+  appId: '1:120019969623:web:6d6ba9a3d0834b0259e512'
+});
+
 const startGameService = async ({ numberOfTeams, topicPackUid }) => {
   const startGame = firebase.functions().httpsCallable('startGame');
 
