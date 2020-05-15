9d0760c87583f38162e5cab6d44147b4da96feda
Andrew Sutherland <azsuth@gmail.com>
2020-05-14 22:57:33 -0500

    add instructions

--- web/src/components/home/index.js
+++ web/src/components/home/index.js
@@ -4,11 +4,12 @@ import { Button } from '@material-ui/core';
 
 import withRouter, { toJoin, toCreate } from 'utilities/router';
 import { withAction } from '@state';
-import { clearState } from '@actions';
+import { clearState, showCoachmark } from '@actions';
 
 import Logo from 'components/shared/logo';
+import Instructions from 'components/home/instructions';
 
-const Home = ({ clearState, routes: [toJoin, toCreate] }) => {
+const Home = ({ clearState, routes: [toJoin, toCreate], showCoachmark }) => {
   useEffect(() => {
     clearState();
   }, []);
@@ -19,12 +20,30 @@ const Home = ({ clearState, routes: [toJoin, toCreate] }) => {
         <Logo />
       </div>
       <div class="home__buttons">
-        <Button variant="contained" disableElevation onClick={toJoin}>
+        <Button
+          className="home__main-button"
+          variant="contained"
+          disableElevation
+          onClick={toJoin}
+        >
           Join an existing game
         </Button>
         <span class="home__separator">or</span>
-        <Button variant="contained" disableElevation onClick={toCreate}>
+        <Button
+          className="home__main-button"
+          variant="contained"
+          disableElevation
+          onClick={toCreate}
+        >
           Start a new one
+        </Button>
+        <span class="home__separator" />
+        <Button
+          className="home__secondary-button"
+          variant="outlined"
+          onClick={() => showCoachmark(<Instructions />)}
+        >
+          How to play
         </Button>
       </div>
     </div>
@@ -33,9 +52,10 @@ const Home = ({ clearState, routes: [toJoin, toCreate] }) => {
 
 // actions
 const withClearStateAction = withAction(clearState, 'clearState');
+const withShowCoackmarkAction = withAction(showCoachmark, 'showCoachmark');
 
 // routes
 const withRoutes = withRouter(toJoin, toCreate);
 
 export { Home };
-export default withClearStateAction(withRoutes(Home));
+export default withClearStateAction(withShowCoackmarkAction(withRoutes(Home)));
---
+++ web/src/components/home/instructions.js
@@ -0,0 +1,43 @@
+import { h } from 'preact';
+
+import Logo from 'components/shared/logo';
+
+const Instructions = () => (
+  <div>
+    <Logo size="small" />
+    <br />
+    Top Four is a party game in which you rank random things and your friends
+    guess how you ranked them. Do you like mayonnaise more than small dogs? Are
+    you secretly Kenny G's biggest fan? Is coffee not your cup of tea? Let's
+    find out!
+    <h2>How to play</h2>
+    The game is broken into multiple rounds. In each round, a player is chosen
+    at random to be the "Ranker." If you're the Ranker, you'll receive four
+    trivial yet polarizing topics to rank from best to worst based on your
+    personal preference.
+    <br />
+    <br />
+    For example, you may see: Road Trips, Rainy Sundays, Dave Matthews Band, and
+    Scrambled Eggs. Rank them based on how you feel about each one and "lock in"
+    your response. Be sure to keep your response a secret from the other
+    players!
+    <br />
+    <br />
+    All other players in the game will try to guess how you responded, then
+    "lock in" on their own screens.
+    <br />
+    <br />
+    Once everyone is locked in, you reveal your responses! Have fun with it!
+    <br />
+    <br />
+    After revealing all four topics, a new round begins and a new Ranker is
+    assigned. The game continues until there are no more topics left to rank.
+    <h2>How to win</h2>
+    Players get one point for each topic that they place in the exact same
+    position as the Ranker. The player with the most points at the end of the
+    game wins!
+  </div>
+);
+
+export { Instructions };
+export default Instructions;
